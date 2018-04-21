b561f80a5b743d1929284587b7609f0bbc2ace67
Aisatou Diallo <diallo.aisatou@gmail.com>
2018-04-21 02:48:23 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -233,20 +233,20 @@ def most_points_scored
 end
 
 def winning_team
-+  total_points = 0
-+  win_team = ''
-+  game_hash.each do |home_away, keys|
-+    team_points = 0
-+    team_name = game_hash[home_away][:team_name]
-+    keys[:players].each do |player|
-+      points = player[:points]
-+      team_points += points
-+    end
-+    win_team, total_points = team_name, team_points if team_points > total_points
-+  end
-+  return win_team
-+end
-+
+  total_points = 0
+  win_team = ''
+  game_hash.each do |home_away, keys|
+   team_points = 0
+    team_name = game_hash[home_away][:team_name]
+    keys[:players].each do |player|
+     points = player[:points]
+      team_points += points
+   end
+    win_team, total_points = team_name, team_points if team_points > total_points
+  end
+  return win_team
+end
+
 +def player_with_longest_name
 +  longest = ''
 +  longest_length = 0
@@ -261,14 +261,14 @@ def winning_team
 +
 +
 +# Super Bonus
-+
-+def long_name_steals_a_ton?
-+  steals_most = ''
-+  most_steals = 0
-+  game_hash.each do |home_away, keys|
-+    keys[:players].each do |player|
-+      steals_most, most_steals = player[:player_name], player[:steals] if player[:steals] > most_steals
+
+def long_name_steals_a_ton?
+  steals_most = ''
+  most_steals = 0
+  game_hash.each do |home_away, keys|
+    keys[:players].each do |player|
+      steals_most, most_steals = player[:player_name], player[:steals] if player[:steals] > most_steals
 +    end
-+  end
-+  return true if steals_most == player_with_longest_name
+  end
+  return true if steals_most == player_with_longest_name
 +end
