9bb4b73d9182e93c7c31e9b30f15c48fc0986346
Aisatou Diallo <diallo.aisatou@gmail.com>
2018-04-21 02:51:40 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -262,15 +262,13 @@ end
 #     return longest_name
 # end
 
-# # Super Bonus
-
 # def long_name_steals_a_ton?
 #   steals_most = ''
 #   most_steals = 0
 #   game_hash.each do |home_away, keys|
 #     keys[:players].each do |player|
 #       steals_most, most_steals = player[:player_name], player[:steals] if player[:steals] > most_steals
-# +    end
+#     end
 #   end
 #   return true if steals_most == player_with_longest_name
-# +end
+# end
